d05f5467241bf0950409baf24e3d09f5b1252da6
Nathapat Prompasit <plwe1890@gmail.com>
2022-01-30 17:15:42 +0700

    1.0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carcare/open_screen.dart';
+import 'package:carcare/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
